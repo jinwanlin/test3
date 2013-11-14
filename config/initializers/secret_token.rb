07952ef234db3af8266f86ebb3d1c1d9f2cbd7e6
jinwanlin123@gmail.com <jinwanlin123@gmail.com>
2013-11-14 23:30:11 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foods::Application.config.secret_key_base = 'f26b0241bd5fa14926b2359285a22158c20e8a8993cacde911d873b6b1fee2e331e805a01eb67594d3248c9379abe0dd3da824fe3da99b151f86606d89018914'
