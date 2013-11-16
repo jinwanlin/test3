0870e4264a68f18cdc75a9cc05979ca55d807863
jinwanlin123@gmail.com <jinwanlin123@gmail.com>
2013-11-16 09:48:06 +0800

    sd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test5::Application.config.secret_token = '97a69eef2547c0c27b4b7fed079f3e7ceba49730addeb596235fe0a1a65c54f606feaa6928e24d5060de4006cc2ad413d2369bf2f8b3809aa73ab046e9cc10c9'
