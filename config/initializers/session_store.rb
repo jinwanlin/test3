a55459e06d42b482110bc76e2f83e3a02b69a8ba
jinwanlin123@gmail.com <jinwanlin123@gmail.com>
2013-11-16 21:44:36 +0800

    s

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Test5::Application.config.session_store :cookie_store, key: '_test5_session'
+Test5::Application.config.session_store :cookie_store, key: '_Test5_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
