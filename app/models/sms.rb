3bad9da4b619ac0f6df74acd1cca88de74147ef0
jinwanlin123@gmail.com <jinwanlin123@gmail.com>
2014-03-24 18:46:28 +0800

    debug

--- app/models/sms.rb
+++ app/models/sms.rb
@@ -4,7 +4,7 @@ class SMS
    MSG={"-1" => "没有该用户账户", "-2" => "密钥不正确", "-3" => "短信数量不足", "-11" => "该用户被禁用", "-14" => "短信内容出现非法字符", "-4" => "手机号格式不正确", "-41" => "手机号码为空", "-42" => "短信内容为空", "-51" => "短信签名格式不正确, 接口签名格式为：【签名内容】"}
   
   def self.send(phone, content)
-    p Socket.gethostname
+    p content
     if Socket.gethostname == "AY131203213614306c1aZ_" #只有服务器才发送短信
       url = URI.escape("http://utf8.sms.webchinese.cn/?Uid=wanlinjin&Key=caa74871061793420615&smsMob=#{phone}&smsText=#{content}")
       code = open(url).read.to_i
