db0a5d86266e7a50cae32d4fde0217a927b72beb
jinwanlin123@gmail.com <jinwanlin123@gmail.com>
2014-04-16 09:03:48 +0800

    a

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -56,7 +56,7 @@ class ProductsController < ApplicationController
     @products = Product.where("state in (?)", @states)
     @products = @products.where(type: params[:type]) if params[:type].present?
     @products = @products.where(optional_amounts: params[:optional_amounts]) if params[:optional_amounts].present?
-    @products = @products.order("cost") #.paginate(:page => @page, :per_page => 5)
+    @products = @products.order("pinyin") #.paginate(:page => @page, :per_page => 5)
   end
   
   def sortable
